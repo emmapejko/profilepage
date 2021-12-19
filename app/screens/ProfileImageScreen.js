87783c3490ed8c8e258f54ec41a7549e6dd127ae
emmapejko <emma.pejko@gmail.com>
2021-12-19 14:17:59 -0600

    feat: user can upload image for profile picture

--- app/screens/ProfileImageScreen.js
+++ app/screens/ProfileImageScreen.js
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Pressable, Button, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 const ProfileImageScreen = ({ navigation, route }) => {
   const { name, number, email, bio, image } = route.params;
 
   const [newImage, setNewImage] = useState(image);
+
+  const openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setNewImage(pickerResult.uri);
+  }
 
   return (
     <>
@@ -16,9 +34,12 @@ const ProfileImageScreen = ({ navigation, route }) => {
     </View>
     <View style={styles.container}>
       <Text>Upload a photo of yourself:</Text>
-        <Image
-          source={ newImage ? { uri: newImage } : require('../assets/profilepic.png')}
-        />
+        <Pressable onPress={openImagePickerAsync}>
+          <Image
+            style={styles.thumbnail}
+            source={ newImage ? { uri: newImage } : require('../assets/profilepic.png')}
+          />
+        </Pressable>
       <Button
         title="Update"
         onPress={() => navigation.navigate('Home', { image: newImage, number: number, name: name, email: email, bio: bio })}
@@ -37,6 +58,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
   }
 })
 
