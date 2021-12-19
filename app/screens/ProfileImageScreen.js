89a182ae19f3adfaa5acb3c0a159a8ec899fee2c
emmapejko <emma.pejko@gmail.com>
2021-12-19 15:56:53 -0600

    fix: formatting & styling for image screen

--- app/screens/ProfileImageScreen.js
+++ app/screens/ProfileImageScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Pressable, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
 
@@ -26,26 +26,28 @@ const ProfileImageScreen = ({ navigation, route }) => {
   }
 
   return (
-    <>
+    <ScrollView keyboardShouldPersistTaps="handled">
     <View style={styles.backButton}>
       <Pressable onPress={() => navigation.navigate('Home', { number : number, name: name, email: email, bio: bio, image: image })}>
         <Ionicons name="arrow-back" size={24} color="black" />
       </Pressable>
     </View>
     <View style={styles.container}>
-      <Text>Upload a photo of yourself:</Text>
+      <Text style={styles.header}>Upload a photo of yourself:</Text>
         <Pressable onPress={openImagePickerAsync}>
           <Image
             style={styles.thumbnail}
             source={ newImage ? { uri: newImage } : require('../assets/profilepic.png')}
           />
         </Pressable>
-      <Button
-        title="Update"
+      <Pressable
         onPress={() => navigation.navigate('Home', { image: newImage, number: number, name: name, email: email, bio: bio })}
-      />
+        style={styles.button}
+      >
+        <Text style={styles.update}>Update</Text>
+      </Pressable>
     </View>
-    </>
+    </ScrollView>
   )
 }
 
@@ -63,7 +65,24 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     resizeMode: "contain"
-  }
+  },
+  header: {
+    alignSelf: "flex-start",
+    padding: 30,
+    fontSize: 25,
+  },
+  button: {
+    marginTop: '20%',
+    width: '80%',
+    height: 50,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  update: {
+    color: 'white',
+    fontWeight: 'bold'
+  },
 })
 
 export default ProfileImageScreen;
